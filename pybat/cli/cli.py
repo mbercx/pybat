30176cbe4b41f697cbc0ebfaa1583c94de174f4c
Marnik Bercx <mbercx@gmail.com>
2017-10-11 17:04:10 +0200

    Cleanup cli.py

--- pybat/cli/cli.py
+++ pybat/cli/cli.py
@@ -9,12 +9,14 @@ Command line interface for the pybat package.
 # This is used to make '-h' a shorter way to access the CLI help
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
+
 @click.group(context_settings=CONTEXT_SETTINGS)
 def main():
     """
     CLI tools for performing calculations for studying batteries.
     """
     pass
+
 
 @main.group(context_settings=CONTEXT_SETTINGS)
 def define():
@@ -23,6 +25,7 @@ def define():
     """
     pass
 
+
 @define.command(context_settings=CONTEXT_SETTINGS)
 def migration():
     """
@@ -30,6 +33,7 @@ def migration():
     structure.
     """
     pass
+
 
 @main.group(context_settings=CONTEXT_SETTINGS)
 def setup():
@@ -45,14 +49,14 @@ def setup():
                    "first step in the transition path determination. Note "
                    "that this directory has to contain the structure files "
                    "for the initial and final state. ")
-@click.option("--migration", "-m", is_flag=True,
+@click.option("--is_migration", "-m", is_flag=True,
               help="Flag to designate the transition as a migration. "
                    "Activating this flag means that a static calculation will "
                    "be set up to determine the charge density of the host "
                    "structure, i.e. without the migrating ion. This charge "
                    "density can then be used to find a good initial guess "
                    "for the migration pathway.")
-def transition(directory, migration):
+def transition(directory, is_migration):
     """
     Set up a the geometry optimizations for the initial and final state of a
     transition.
@@ -67,7 +71,8 @@ def transition(directory, migration):
     set_up_transition(directory=directory,
                       initial_structure=initial_structure,
                       final_structure=final_structure,
-                      is_migration=migration)
+                      is_migration=is_migration)
+
 
 @setup.command(context_settings=CONTEXT_SETTINGS)
 @click.option("--directory", "-d", default=".",
@@ -77,14 +82,14 @@ def transition(directory, migration):
                    "for the initial and final state. ")
 @click.option("--nimages", "-n", default=8,
               help="Number of images.")
-@click.option("--migration", "-m", is_flag=True,
+@click.option("--is_migration", "-m", is_flag=True,
               help="Flag to designate the transition as a migration. "
                    "Activating this flag means that a static calculation will "
                    "be set up to determine the charge density of the host "
                    "structure, i.e. without the migrating ion. This charge "
                    "density can then be used to find a good initial guess "
                    "for the migration pathway.")
-def neb(directory, nimages, migration):
+def neb(directory, nimages, is_migration):
     """
     Set up the Nudged Elastic Band calculation based on the output in the
     initial and final directory.
@@ -96,4 +101,4 @@ def neb(directory, nimages, migration):
 
     set_up_NEB(directory=directory,
                nimages=nimages,
-               is_migration=migration)
+               is_migration=is_migration)
