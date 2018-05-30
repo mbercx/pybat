f06db999af96bcad007a42fd1aa32d07f6f12c8f
Marnik Bercx <mbercx@gmail.com>
2018-05-30 10:06:10 +0200

    Adjusted pybat define migration to be more flexible and user friendly

--- pybat/cli/commands/define.py
+++ pybat/cli/commands/define.py
@@ -27,24 +27,75 @@ def define_migration(structure_file, write_cif=False):
     coordinates to which the site will migrate.
 
     """
-    initial_structure = Cathode.from_file(structure_file)
-    final_structure = initial_structure.copy()
+    cathode = Cathode.from_file(structure_file)
+    final_structure = cathode.copy()
 
     # Ask the user for the migration site
     print("")
-    print(initial_structure)
+    print(cathode)
     print("")
     migration_site_index = int(input("Please provide the index of the "
-                                     "migrating cation:\n"))
-    migration_species = initial_structure.sites[migration_site_index].specie
+                                     "migrating cation (Note: Not the VESTA "
+                                     "index!): "))
+    print("")
+
+    migration_site = cathode.sites[migration_site_index]
+    migration_species = migration_site.specie
+
+    # Check if the site to which the ion should migrate is actually occupied
+    if migration_site in cathode.cation_sites and migration_site not in \
+            cathode.cation_configuration:
+        raise ValueError("Chosen site is vacant.")
 
     # Ask the user for the final coordinates of the migrating ion
-    final_coords = input("Please provide the final fractional coordinates "
-                         "of the migrating site:\n")
+    final_coords = input("Please provide the index of the site the cation "
+                         "is migrating to, or the final fractional coordinates "
+                         "of the migration site: ")
+    print("")
     final_coords = [float(number) for number
                     in list(final_coords.split(" "))]
 
-    # Remove the original site from the final structure
+    # In case of a site index as input
+    if len(final_coords) == 1:
+
+        # Grab the required information about the final site
+        final_site_index = final_coords[0]
+        final_site = cathode.sites[final_site_index]
+        final_coords = final_site.frac_coords
+        final_species = final_site.specie
+
+        # Check if site is occupied
+        if final_site not in cathode.cation_sites or final_site in \
+                cathode.cation_configuration:
+            raise ValueError("Chosen final site is not vacant.")
+
+        # Change the coordinates of the migration site with the ones of
+        # the final site.
+        final_structure.replace(i=migration_site_index,
+                                species=migration_species,
+                                coords=final_coords,
+                                properties=migration_site.properties)
+
+        # Do the opposite for the final site
+        final_structure.replace(i=final_site_index,
+                                species=final_species,
+                                coords=migration_site.coords,
+                                properties=final_site.properties)
+
+    # In case of a set of fractional coordinates as input
+    elif len(final_coords) == 3:
+
+        # Replace the site with the site of the new coordinates
+        final_structure.replace(i=migration_site_index,
+                                species=migration_species,
+                                coords=final_coords,
+                                properties=final_structure.sites[
+                                    migration_site_index].properties)
+
+    else:
+        raise IOError("Input is incorrect.")
+
+    # Replace the
     final_structure.remove_sites([migration_site_index])
 
     # Add the final position of the migrating ion
@@ -52,16 +103,20 @@ def define_migration(structure_file, write_cif=False):
                            species=migration_species,
                            coords=final_coords,
                            properties=final_structure.sites[
-                                migration_site_index].properties)
+                               migration_site_index].properties)
+
+    initial_structure_file = "".join(structure_file.split(".")[0:-1]) + "_init"
+
+    final_structure_file = "".join(structure_file.split(".")[0:-1]) + "_final"
 
     # Write out the initial and final structures
-    initial_structure.to("json", "init.json")
-    final_structure.to("json", "final.json")
+    cathode.to("json", initial_structure_file + ".json")
+    final_structure.to("json", final_structure_file + ".json")
 
     # Write the structures to a cif format if requested
     if write_cif:
-        initial_structure.to("cif", "init.cif")
-        final_structure.to("cif", "final.cif")
+        cathode.to("cif", initial_structure_file + ".cif")
+        final_structure.to("cif", final_structure_file + ".cif")
 
 
 def define_dimer(structure_file, dimer_indices=(0, 0), distance=0,
@@ -99,11 +154,11 @@ def define_dimer(structure_file, dimer_indices=(0, 0), distance=0,
         cathode.remove_dimer_cations(dimer_indices)
 
     dimer_structure_file = structure_file.split(".")[0] + "_dimer_init" \
-        + ".json"
+                           + ".json"
     cathode.to("json", dimer_structure_file)
 
     cathode.change_site_distance(dimer_indices, distance)
 
     dimer_structure_file = structure_file.split(".")[0] + "_dimer_final" \
-        + ".json"
+                           + ".json"
     cathode.to("json", dimer_structure_file)
