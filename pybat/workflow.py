1353aae0aa668b9ba459bff9c1e956beb292a3db
mbercx <marnik.bercx@uantwerpen.be>
2018-12-30 19:57:47 +0100

    disabled parts for vasp to run on breniac, as they are currently not functional and interfering with the proper functioning of the vasp_run script on Leibniz

--- pybat/workflow.py
+++ pybat/workflow.py
@@ -95,14 +95,6 @@ def run_vasp(directory, number_nodes):
 
     """
     # Workaround for making number of nodes work on breniac #TODO
-    # number = ""
-    #
-    # for c in number_nodes:
-    #     if c.isalnum():
-    #         number += c
-
-    # with open("lala", "w") as file:
-    #     file.write(VASP_RUN_SCRIPT)
 
     os.chdir(directory)
     subprocess.call(VASP_RUN_SCRIPT)
