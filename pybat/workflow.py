fa4a76da2747772a3a7ee3efac7c639bd61a4a0b
mbercx <marnik.bercx@uantwerpen.be>
2019-01-05 10:48:06 +0100

    attempt to use fireworker env variable to distinguish cluster prep commands and main vasp command

--- pybat/workflow.py
+++ pybat/workflow.py
@@ -109,7 +109,8 @@ class VaspTask(FiretaskBase):
     def run_task(self, fw_spec):
 
         os.chdir(self["directory"])
-        subprocess.call(VASP_RUN_SCRIPT)
+        subprocess.call(fw_spec["_fw_env"]["vasp_prep"])
+        subprocess.call(fw_spec["_fw_env"]["vasp_command"])
 
 
 class CustodianTask(FiretaskBase):
