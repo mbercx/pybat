2c427a9d163315e9aeb3c4054c0facb0ccba6d3b
mbercx <marnik.bercx@uantwerpen.be>
2019-02-20 09:44:02 +0100

    debugging

--- pybat/cli/commands/setup.py
+++ pybat/cli/commands/setup.py
@@ -370,6 +370,7 @@ def neb(directory, functional=("pbe", {}), nimages=8, is_metal=False,
         images_2 = middle_structure.interpolate(end_structure=final_structure,
                                                 nimages=int((nimages)/2),
                                                 interpolate_lattices=True)
+        pdb.set_trace()
         images = images_1 + images_2[1:]
 
 
