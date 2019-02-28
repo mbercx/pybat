726230c20de3071ce2c42b6ee519a56b7046cbdb
mbercx <marnik.bercx@uantwerpen.be>
2019-02-28 15:37:17 +0100

    added create_relax_fw method, allowing the workflows to create a geometry optimization Firework by simply calling this method instead of setting up the FireTasks inside the workflow.

--- pybat/workflow.py
+++ pybat/workflow.py
@@ -287,8 +287,69 @@ def create_scf_fw(structure_file, functional, directory, write_chgcar, in_custod
     return scf_firework
 
 
-def create_neb_fw(directory, nimages, functional, is_metal, is_migration, in_custodian,
-                  number_nodes):
+def create_relax_fw(structure_file, functional, directory, is_metal,
+                    in_custodian, number_nodes):
+    """
+    Create a FireWork for performing an SCF calculation.
+
+    Args:
+        structure_file (str): Path to the geometry file of the structure.
+        functional (tuple): Tuple with the functional choices. The first element
+            contains a string that indicates the functional used ("pbe", "hse", ...),
+            whereas the second element contains a dictionary that allows the user
+            to specify the various functional tags.
+        directory (str): Directory in which the SCF calculation should be performed.
+        is_metal (bool): Flag that indicates the material being studied is a
+            metal, which changes the smearing from Gaussian to second order
+            Methfessel-Paxton of 0.2 eV.
+        in_custodian (bool): Flag that indicates whether the calculation should be
+            run inside a Custodian.
+        number_nodes (int): Number of nodes that should be used for the calculations.
+            Is required to add the proper `_category` to the Firework generated, so
+            it is picked up by the right Fireworker.
+
+    Returns:
+        Firework: A firework that represents an SCF calculation.
+
+    """
+    # Create the PyTask that sets up the calculation
+    setup_relax = PyTask(
+        func="pybat.cli.commands.setup.relax",
+        kwargs={"structure_file": structure_file,
+                "functional": functional,
+                "calculation_dir": directory,
+                "is_metal": is_metal}
+    )
+
+    # Create the PyTask that runs the calculation
+    if in_custodian:
+        vasprun = CustodianTask(directory=directory)
+    else:
+        vasprun = VaspTask(directory=directory)
+
+    # Create the PyTask that check the Pulay stresses
+    pulay_task = PulayTask(directory=directory,
+                           in_custodian=in_custodian,
+                           number_nodes=number_nodes,
+                           tol=PULAY_TOLERANCE)
+
+    # Only add number of nodes to spec if specified
+    firework_spec = {"_launch_dir": os.getcwd()}
+    if number_nodes == 0:
+        firework_spec.update({"_category": "none"})
+    else:
+        firework_spec.update({"_category": str(number_nodes) + "nodes"})
+
+    # Combine the FireTasks into one FireWork
+    relax_firework = Firework(tasks=[setup_relax, vasprun, pulay_task],
+                              name="Geometry optimization",
+                              spec=firework_spec)
+
+    return relax_firework
+
+
+def create_neb_fw(directory, nimages, functional, is_metal, is_migration,
+                  in_custodian, number_nodes):
     """
     Create a FireWork for performing an NEB calculation.
 
@@ -438,38 +499,13 @@ def relax_workflow(structure_file, functional=("pbe", {}), directory="",
                                        in functional[1]["LDAUU"].keys())
         directory += "_relax"
 
-    # Create the PyTask that sets up the calculation
-    setup_relax = PyTask(
-        func="pybat.cli.commands.setup.relax",
-        kwargs={"structure_file": structure_file,
-                "functional": functional,
-                "calculation_dir": directory,
-                "is_metal": is_metal}
-    )
-
-    # Create the PyTask that runs the calculation
-    if in_custodian:
-        vasprun = CustodianTask(directory=directory)
-    else:
-        vasprun = VaspTask(directory=directory)
-
-    # Create the PyTask that check the Pulay stresses
-    pulay_task = PulayTask(directory=directory,
-                           in_custodian=in_custodian,
-                           number_nodes=number_nodes,
-                           tol=PULAY_TOLERANCE)
-
-    # Only add number of nodes to spec if specified
-    firework_spec = {"_launch_dir": os.getcwd()}
-    if number_nodes == 0:
-        firework_spec.update({"_category": "none"})
-    else:
-        firework_spec.update({"_category": str(number_nodes) + "nodes"})
-
     # Combine the FireTasks into one FireWork
-    relax_firework = Firework(tasks=[setup_relax, vasprun, pulay_task],
-                              name="Geometry optimization",
-                              spec=firework_spec)
+    relax_firework = create_relax_fw(structure_file=structure_file,
+                                     functional=functional,
+                                     directory=directory,
+                                     is_metal=is_metal,
+                                     in_custodian=in_custodian,
+                                     number_nodes=number_nodes)
 
     # Set up a clear name for the workflow
     cathode = LiRichCathode.from_file(structure_file)
