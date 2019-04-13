3c003fc7fc1e4bd89a6f6f1e86077edb0fbf3c1d
mbercx <marnik.bercx@uantwerpen.be>
2019-04-13 10:16:30 +0200

    debugging: problem with static workflow (Reminder: Unittests!)

--- pybat/cli/cli.py
+++ pybat/cli/cli.py
@@ -694,6 +694,8 @@ def static(structure_file, functional, directory, write_chgcar, in_custodian,
         number_nodes = None
     functional = string_to_functional(functional)
 
+    print(functional)
+
     # Set up the calculation directory
     directory = set_up_directory(directory, functional, "static")
 
