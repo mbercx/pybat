26ded1605d37ac170383310f95f661be66b788ac
mbercx <marnik.bercx@uantwerpen.be>
2019-04-18 14:12:16 +0200

    update: pybat module __init__.py Imported core classes into pybat __init__.py so they can be imported straight from the pybat package.

---
+++ pybat/__init__.py
@@ -0,0 +1,10 @@
+# These import commands make importing core classes easier, e.g. you can just import
+# Cathode using:
+#
+# from pybat import Cathode
+#
+# Instead of:
+#
+# from pybat.core import Cathode
+#
+from pybat.core import Cathode, LiRichCathode, Dimer, DimerNEBAnalysis
